cbd8703b4f451c54075884c285234478b99064fa
IdanGib <idangibly.dev@gmail.com>
2021-01-11 10:25:39 +0200

    more data

--- client/pages/api/companies.js
+++ client/pages/api/companies.js
@@ -7,19 +7,6 @@ const companies = [
       { 
         stage: 'phone interview', 
         questions: [
-          'tell me about yourself?',
-          'tell me about the last 3 jobs you had, and why you leave them?'
-        ] 
-      }
-    ]
-  }, { 
-    name: 'talkspace', 
-    site: 'https://www.talkspace.com', 
-    icon: 'https://www.talkspace.com/favicon.ico', 
-    interview: [
-      { 
-        stage: 'phone interview', 
-        questions: [
           'what is session cookie and localStorage, what the difference  between them?',
           'what is arrow function how its difference  from regular function?',
           'what is promise?',
@@ -27,11 +14,36 @@ const companies = [
           'what the keywords \'let\' \'const\' \'var\' means, what the difference  between them?',
           'what async await keywords meaning? how and when we use them?',
         ] 
+
+      }
+    ]
+  }, { 
+    name: 'talkspace', 
+    site: 'https://www.talkspace.com', 
+    icon: 'https://www.talkspace.com/favicon.ico',
+    interview: [
+      { 
+        stage: 'phone interview', 
+        questions: [
+          'Tell me about yourself?',
+          'Tell me about the last 3 jobs you had, and why you leave them?'
+        ] 
+      },
+      { 
+        stage: 'zoom interview ( R&D manager and product manager )', 
+        questions: [
+          'Tell me about yourself?',
+          'Tell me about some big feature you make, what the architecture? data flow ? etc..',
+          'What is nodejs ? how its diffrent from other servers ? what are the pros and cons ?',
+          'code: \n console.log(1);\nsetTimeout(() => console.log(2), 0);\nconsole.log(3); \n\n what is the log order and why ?  ',
+          'code: \n write function \'foo\' that calc multicplication of two numbers: foo(3)(4) => 18',
+          '** In general they ask about almost any technologie that i mention in the inteview'
+        ] 
       }
     ]
   }
 ];
 export default (req, res) => {
-  res.statusCode = 200
-  res.json(companies)
+  res.statusCode = 200;
+  res.json(companies);
 }
--- client/pages/interview.js
+++ client/pages/interview.js
@@ -16,20 +16,25 @@ class InterviewComponent extends React.Component {
         
         return  <>
             
-            <h1 className="text-center text-5xl capitalize p-5">       
-                { this.state.company ? this.state.company.name : '' } 
-            </h1>
-            <div className="text-2xl italic opacity-40 text-center capitalize p-5">
-                 interview path
-            </div> 
-            <div className='flex flex-col items-center py-6'>
+            <div className="flex flex-col items-center capitalize p-5">
+                <div>
+                    <div className='text-5xl'>{ this.state.company ? this.state.company.name : '' }</div> 
+                    <div className="text-md opacity-40 capitalize">
+                    interview path
+                    </div>
+                </div>
+            </div>
+ 
+            <div className='flex flex-col items-center'>
  
             <div className='container max-w-lg'>
                 { this.state.company ? this.state.company.interview.map( (i, key) => {
                     return <div className='m-3 p-3 border' key={key}>
                         <h2 className='uppercase'>{i.stage}</h2>
                         <ul>
-                        { i.questions.map((q, k) => <li className='my-1 p-1' key={k}>{q}</li>) }
+                        { i.questions.map((q, k) => (<li className='my-1 p-1 whitespace-pre-line' key={k}>
+                            { q.startsWith('code:') ? <code>{q}</code> : <span>{q}</span> }
+                        </li>)) }
                         </ul>
                     
                     </div>
